c651e050ad389c25aae66cc3e270905151aeb7e1
Nick Weber <webernick@icloud.com>
2019-05-07 06:45:31 -0700

    Adds error handling

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Treehouse Full Stack Javascript Tech Degree
 // PROJECT 06
 
+
 // NODE MODULES
 const express = require('express');
 const bodyParser = require('body-parser');
-
 
 // --------- Express Configuration -------------
 // create a new express app
@@ -20,11 +20,44 @@ app.use('/static', express.static('public'));
 // Use 'pug' for templating
 app.set('view engine', 'pug');
 
+// ------------------- STATIC DATA FILE -----------------
+const data = require('./data.json');
+// const myProjects = data.projects;
+
 // -------------------- ROUTES -------------------
 app.get('/', (req, res) => {
   res.render('about');
 });
 
+// index - photos should be 550 X 550
+app.get('/index', (req, res) => {
+  res.render('index', data);
+});
+
+// project detail
+// project detail - photos should be 1200 X 550
+app.get('/project/:id', (req, res) => {
+  const id = req.params.id;
+  res.render('project', data.projects[id]);
+});
+
+
+
+// --------------- ERROR HANDLING -------------------------
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+  
+// General error handler
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  // call my template, and pass the err object
+  console.log(err);
+  res.render('error', err);
+});
 
 
 // Nodemon development server
